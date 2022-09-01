78afd6a629fb94bf9dacb1815e2f1acbbeea02c2
Bill Zheng <bill.t.zheng@gmail.com>
2022-09-02 07:28:06 +0800

    escape '

--- front-end/pages/about.tsx
+++ front-end/pages/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <div>
-            Touch Leaderboard is a community project that aims to promote usage of Toucan's carbon bridging and retirement functions. Top retirement addresses will be able to claim NFTs and receive various community benefits, such as access to events, limited merch etc.
+            Touch Leaderboard is a community project that aims to promote usage of Toucan&apos;s carbon bridging and retirement functions. Top retirement addresses will be able to claim NFTs and receive various community benefits, such as access to events, limited merch etc.
         </div>
     )
 }
