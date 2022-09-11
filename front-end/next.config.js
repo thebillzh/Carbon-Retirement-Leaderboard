102c06ce16b928ce68c290f7e32cf77dbc0e51d5
Debian Bear <debianbear@gmail.com>
2022-09-11 08:44:16 +0800

    add proxy to avoid cors

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/proxy/:path*",
+        destination: "https://api-go.toucanleader.xyz/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
