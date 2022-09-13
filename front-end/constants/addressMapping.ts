8d12271e03062efa0c830f8e6d17b94c3b43b93b
Bill Zheng <bill.t.zheng@gmail.com>
2022-09-14 02:11:38 +0800

    fix address mapping typo

--- front-end/constants/addressMapping.ts
+++ front-end/constants/addressMapping.ts
@@ -1,6 +1,5 @@
-export const ToucanAddressMapping: {[key:string]:string} = {
-"0xc1e088fc1323b20bcbee9bd1b9fc9546db5624c5": "Beanstalk: Beanstalk Protocol", // etherscan public tag https://etherscan.io/address/0xc1e088fc1323b20bcbee9bd1b9fc9546db5624c5
-"0xCfeC3cfd322a3941c6E64bCc6Ae4a39F1623c325": "Toucan NCT redemption fee wallet" // https://blog.toucan.earth/toucan-burn-report-vol-1-3000-tonnes-of-carbon-for-the-planet/
-
-
-}
+export const ToucanAddressMapping: { [key: string]: string } = {
+  "0xc1e088fc1323b20bcbee9bd1b9fc9546db5624c5": "Beanstalk: Beanstalk Protocol", // etherscan public tag https://etherscan.io/address/0xc1e088fc1323b20bcbee9bd1b9fc9546db5624c5
+  "0xcfec3cfd322a3941c6e64bcc6ae4a39f1623c325":
+    "Toucan NCT Redemption Fee Wallet", // https://blog.toucan.earth/toucan-burn-report-vol-1-3000-tonnes-of-carbon-for-the-planet/
+};
