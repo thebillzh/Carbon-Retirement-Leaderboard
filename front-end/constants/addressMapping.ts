1c7b969a09af2c555a9e6b1e1c4fc35a472a4082
Bill Zheng <bill.t.zheng@gmail.com>
2022-09-14 13:04:28 +0000

    update name mapping

--- front-end/constants/addressMapping.ts
+++ front-end/constants/addressMapping.ts
@@ -2,5 +2,6 @@ export const ToucanAddressMapping: { [key: string]: string } = {
   "0xc1e088fc1323b20bcbee9bd1b9fc9546db5624c5": "Beanstalk: Beanstalk Protocol", // etherscan public tag https://etherscan.io/address/0xc1e088fc1323b20bcbee9bd1b9fc9546db5624c5
   "0xcfec3cfd322a3941c6e64bcc6ae4a39f1623c325":
     "Toucan NCT Redemption Fee Wallet", // https://blog.toucan.earth/toucan-burn-report-vol-1-3000-tonnes-of-carbon-for-the-planet/
-    "0xc05a33b9e3b1fbc1ef480cf701bfd11ed53eb0bc":"Lens Handle Retirements", // promotion with Lens https://twitter.com/ToucanProtocol/status/1538129534280769536?s=20&t=mnLly81JfEDRt1HryZl1RQ
+  "0xc05a33b9e3b1fbc1ef480cf701bfd11ed53eb0bc": "Lens Handle Retirements", // promotion with Lens https://twitter.com/ToucanProtocol/status/1538129534280769536?s=20&t=mnLly81JfEDRt1HryZl1RQ
+  "0x904d01bb87c41f178e5f0aa73c8eaa076c3094b8": "Senkusha"
 };
