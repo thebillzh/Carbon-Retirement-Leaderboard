9c49b53467223973f728c93c658b8e30a1cb4dcc
Bill Zheng <bill.t.zheng@gmail.com>
2022-09-15 11:34:12 +0000

    Add top info bar

--- front-end/components/common/layout.tsx
+++ front-end/components/common/layout.tsx
@@ -1,9 +1,12 @@
+import { ExclamationIcon, InformationCircleIcon, XIcon } from "@heroicons/react/outline";
 import SimpleFooter from "./simpleFooter";
 import SimpleHeader from "./simpleHeader";
+import SimpleInfoBar from "./simpleInfoBar";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <div className="w-full h-full mx-auto flex-1 flex flex-col">
+ <SimpleInfoBar/>
       <SimpleHeader />
       <main className="relative w-full flex-1 flex flex-col">{children}</main>
       <SimpleFooter />
--- front-end/components/common/simpleHeader.tsx
+++ front-end/components/common/simpleHeader.tsx
@@ -158,7 +158,7 @@ export default function SimpleHeader() {
                         login();
                       }}
                       type="button"
-                      className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+                      className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-teal-500"
                     >
                       Connect Wallet
                     </button>
